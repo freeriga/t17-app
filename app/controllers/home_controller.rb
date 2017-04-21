f6bfa8742991b2004ebe09e5478ce02e6b6b1f6a
Mikael Brockman <mikael@brockman.se>
2017-04-21 08:37:48 +0300

    resident sort order on index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @projects = Project.all
-    @residents = User.where(admin: true)
+    @residents = User.where(admin: true).order(:name)
     @snippet = Config.find_or_create_by(key: "home-snippet").value
   end
 end
