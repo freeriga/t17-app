c41d0613077acdcc251be0bb4eb76375b244027b
Mikael Brockman <mikael@brockman.se>
2017-04-22 18:59:58 +0300

    mobile friendlier, better layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def markdown(text)
     Redcarpet::Markdown.new(Redcarpet::Render::HTML.new).render(text).html_safe
   end
+
+  def ghost_span(condition, &x)
+    content_tag :span, class: condition ? nil : "ghost", &x
+  end
 end
