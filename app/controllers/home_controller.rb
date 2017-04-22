68116b5654f12b63a42f980907a7eab6993faa70
Mikael Brockman <mikael@brockman.se>
2017-04-22 17:28:30 +0300

    order projcets by name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @projects = Project.all
+    @projects = Project.all.order(:name)
     @residents = User.where(admin: true).order(:name)
     @snippet = Config.find_or_create_by(key: "home-snippet").value
   end
